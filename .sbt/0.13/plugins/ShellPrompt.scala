bc0beae1433d3040536b39e732aef4b4dc0e8ca9
Dave Rostron <yastero@gmail.com>
2014-06-15 16:12:16 -0700

    adding sbt

---
+++ .sbt/0.13/plugins/ShellPrompt.scala
@@ -0,0 +1,29 @@
+import sbt._
+import Keys._
+import scala.Console._
+
+// lives at ~/.sbt/plugins/ShellPrompt.scala
+
+object ShellPrompt extends Plugin {
+  override def settings = Seq(
+    shellPrompt <<= name(name => { state =>
+      object devnull extends ProcessLogger {
+        def info(s: => String) {}
+        def error(s: => String) {}
+        def buffer[T](f: => T): T = f
+      }
+      val current = """\*\s+([\w-./]+)""".r
+      def gitBranches = ("git branch --no-color".lines_!(devnull).mkString)
+      // println("gitBranches : " + gitBranches)
+      // println("current findFirstMatchIn gitBranches : " + current.findFirstMatchIn(gitBranches))
+      // access project name : Project.extract(state).currentRef.project
+      "%s%s%s:%s%s%s> " format (
+        YELLOW,
+        name,
+        RESET,
+        CYAN,
+        current findFirstMatchIn gitBranches map (_.group(1)) getOrElse "-",
+        RESET)
+    })
+  )
+}
