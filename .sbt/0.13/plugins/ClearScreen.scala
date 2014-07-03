a6c45223ba1a0e4cb69f78c8ff9af14f559dab89
Dave Rostron <yastero@gmail.com>
2014-07-02 18:43:14 -0700

    adding clear-scrollback command to ClearScreen sbt plugin

--- .sbt/0.13/plugins/ClearScreen.scala
+++ .sbt/0.13/plugins/ClearScreen.scala
@@ -2,10 +2,14 @@ import sbt._, Keys._
 
 object ClearScreen extends Plugin {
 
-  override def settings = Seq(commands +=
+  override def settings = Seq(commands ++= Seq(
     Command.command("clear-screen") { state ⇒
-      println("\033c")
+      print("\033c")
       state
-    })
+    },
+    Command.command("clear-scrollback") { state ⇒
+      print("\033]50;ClearScrollback" + 7.toChar) // printf '\e]50;ClearScrollback\a'
+      state
+    }))
 
 }
