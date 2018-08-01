20401a05085f0c947ac9e6903baac99f3224445e
Dave Rostron <yastero@gmail.com>
2018-08-01 16:16:01 -0600

    Update nix channels overlay

--- .config/nixpkgs/overlays/channels.nix
+++ .config/nixpkgs/overlays/channels.nix
@@ -2,9 +2,15 @@ self: super:
 {
   local = import ~/code/nixpkgs {};
 
-  unstable = import (builtins.fetchTarball
-    https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
-  ) {};
+  pinned = import (builtins.fetchGit {
+    url = "git://github.com/nixos/nixpkgs";
+    rev = "d7d31fea7e7eef8ff4495e75be5dcbb37fb215d0";
+  }) {};
+
+  unstable = import (builtins.fetchGit {
+    url = "git://github.com/NixOS/nixpkgs-channels";
+    ref = "nixos-unstable";
+  }) {};
 
   master = import (builtins.fetchGit {
     url = "git://github.com/nixos/nixpkgs";
